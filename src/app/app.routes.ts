3d41bfc61998ed493b159094565a3a64897b3ef0
Nethsara Sandeepa Elvitigala <nethsarasandeepaelvitigala@gmail.com>
2024-10-05 19:45:15 +0530

    feat: implement home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,8 @@
 import { dashboardRoutes } from '@features/dashboard/dashboard.routes';
 
+import { homeRoutes } from './features/home/home.routes';
+
 export const appRoutes = [
+    ...homeRoutes,
     ...dashboardRoutes
 ];
