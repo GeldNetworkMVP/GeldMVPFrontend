d10df30eea68dbca339defd7532d3da7fa932809
Keleigh123 <luciaberenger@gmail.com>
2024-11-09 20:20:36 +0000

    updating output path

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist/geld-fe/browser')));
+app.use(express.static(path.join(__dirname, 'dist/geld-fe')));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'dist/geld-fe/browser', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist/geld-fe', 'index.html'));
 });
 
 const port = process.env.PORT || 3000;
